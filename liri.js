19158800f0dc19d06cecb711f82e9ed67f406e91
Brendan Howell <bhowell841@gmail.com>
2019-02-20 18:06:08 -0500

    omdb, spotify are good.  concert broke

--- liri.js
+++ liri.js
@@ -43,8 +43,8 @@ function setName() {
     } // end for
 } // end setMedia
 
-function concertThis() {
-    console.log("ConcertThis is working");
+function concertThis(name) {
+    // console.log("ConcertThis is working");
     if (name === "") {
         name = "Trampled By Turtles";
     }
@@ -54,15 +54,22 @@ function concertThis() {
 
     // get the data
     axios.get(search).then(function (response) {
-        console.log(response.data[0].venue.city);
-        console.log(response.data[0].venue.name);
+        console.log("Artist: " + name);
+        console.log("-----------------------------------");
+        // date = response.data[0].datetime.split("T").slice(0, 1).join(" ");
+        for (var i = 0; i < 10; i++) {
+            console.log(response.data[i].venue.city);
+            console.log(response.data[i].venue.name);
+            console.log(response.data[i].datetime.split("T").slice(0, 1).join(" "));
+            console.log("-----------------------------------");
+        }
     })
 } // end function
 
 
 
 function spotifyThis(name) {
-    console.log("SpotifyThis is working");
+    // console.log("SpotifyThis is working");
     if (name === "") {
         name = "Never gonna give you up";
     }
@@ -73,20 +80,26 @@ function spotifyThis(name) {
     spotify.search({
         type: 'track',
         query: name,
-        limit: 2
+        limit: 1
     }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
 
-        console.log(JSON.stringify(data, null, 2));
+        // console.log(JSON.stringify(data, null, 2));
+        console.log("-----------------------------------");
+        console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+        console.log("Song: " + data.tracks.items[0].name);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("Sample: " + data.tracks.items[0].preview_url);
+        console.log("-----------------------------------");
     });
 } // end function
 
 
 
 function movieThis(name) {
-    console.log("MovieThis is working");
+    // console.log("MovieThis is working");
     if (name === "") {
         name = "Layer Cake";
     }
@@ -97,14 +110,20 @@ function movieThis(name) {
     // get the data
     axios.get(search).then(function (response) {
 
+        console.log("-----------------------------------");
         console.log("Movie: " + response.data.Title);
         console.log("Year: " + response.data.Released);
         console.log("IMDB Rating: " + response.data.imdbRating);
-        console.log("Rotten Tomatos Rating: " + response.data.tomatoRating);
+        if (response.data.Ratings[1]) {
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        } else {
+            console.log("Rotten Tomatoes Rating: None");
+        }
         console.log("Country: " + response.data.Country);
         console.log("Language: " + response.data.Language);
         console.log("Plot: " + response.data.Plot);
         console.log("Actors: " + response.data.Actors);
+        console.log("-----------------------------------");
     });
 }; // end function
 
@@ -112,7 +131,7 @@ function movieThis(name) {
 
 
 function doThis() {
-    console.log("This is running...");
+    // console.log("This is running...");
     fs.readFile("random.txt", "utf8", function (err, data) {
         if (err) {
             console.log("There was an error: " + err);
@@ -120,7 +139,7 @@ function doThis() {
             console.log(data);
 
             var dataArr = data.split(",");
-            var dataArr= dataArr.slice(1).join(" ");
+            var dataArr = dataArr.slice(1).join(" ");
             console.log(dataArr);
 
             spotifyThis(dataArr);
